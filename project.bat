ec0a1fedb532d87718785329e39aa49409056666
alimalik1397 <32353715+alimalik1397@users.noreply.github.com>
2021-06-02 14:20:56 +0500

    Add files via upload

---
+++ project.bat
@@ -0,0 +1,3 @@
+cd/d D:\Data Science\Big Data Dice\Project
+C:\Users\Talha\Anaconda3\python.exe D:\Data Science\Big Data Dice\Project\project_py_3.py
+pscp -pw "Alimalik-1397" D:\Data Science\Big Data Dice\Project\*.json root@192.168.139.128:/root/tweet_recieved && move D:\Data Science\Big Data Dice\Project\* .json D:\Data Science\Big Data Dice\Processed
